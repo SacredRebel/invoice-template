8336ab1e1338fb1769f5b0560c592738a231f629
SacredRebel <paulmuresan77@gmail.com>
2026-08-10 10:17:10 -0700

    fix: server-render settings, no loading flash

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,3 +1,9 @@
+import { getBusiness, getClients, canSave } from "@/lib/data";
 import SettingsEditor from "@/components/SettingsEditor";
+
 export const dynamic = "force-dynamic";
-export default function SettingsPage() { return <SettingsEditor />; }
+
+export default async function SettingsPage() {
+  const [business, clients] = await Promise.all([getBusiness(), getClients()]);
+  return <SettingsEditor business={business} clients={clients} connected={canSave()} />;
+}
